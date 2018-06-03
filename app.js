36b5dc7dda55fcd7f2cff82411608d4b6154d71c
Huynh Dat <huynhdat@Huynhs-MacBook-Air.local>
2018-06-03 21:32:28 +0700

    update

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-var firstName = "Linh";
-(function(lastName) {
-    var firstName = "Khang";
-    console.log(firstName);
-    console.log(lastName);
-}("Rua")); // thuc thi ngay ham khi duoc khoi ta
-
-console.log(firstName);
-
-// chuong trinh in ra 
-// Khang
-// Rua
-// Linh
+var sayHello = require("./hello");
+sayHello();
